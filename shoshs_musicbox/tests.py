508a1bee3232c9464392929f19920e8d5764b4af
Shosh Seiden <shosh.j.seiden@gmail.com>
2015-10-28 17:16:06 -0600

    fixed url namespace

--- shoshs_musicbox/tests.py
+++ shoshs_musicbox/tests.py
@@ -11,12 +11,12 @@ class ViewsTest(TestCase):
 
     def test_artist_library(self):
         Artist.objects.create(artist_name="Test Artist")
-        url = reverse('musicbox:artistLibrary')
+        url = reverse('artistLibrary')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_blog_list(self):
         Blog.objects.create(blog_title="Blog Test", blog_date="2015-10-28 13:40", blog_post="Blah")
-        url = reverse('musicbox:blogList')
+        url = reverse('blogList')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)

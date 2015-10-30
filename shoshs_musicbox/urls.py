f07b70f2f6c289201945e3e7680d2975d30b310d
Shosh Seiden <shosh.j.seiden@gmail.com>
2015-10-30 13:21:24 -0600

    updates to project files

--- shoshs_musicbox/urls.py
+++ shoshs_musicbox/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='home'),
     url(r'^artistlibrary/$', views.ArtistLibraryView.as_view(), name='artist_library'),
     url(r'^artistlibrary/artist/(?P<artist_id>[0-9]+)/$', views.artist, name='artist'),
     url(r'^artist/(?P<artist_id>[0-9]+)/album/(?P<album_id>[0-9]+)/$', views.album, name='album'),
